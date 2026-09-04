794540c0e412ce4fc13e91211e1179d427aa043a
VICTORIA AMORIM DOS SANTOS <victoria.asantos13@senacsp.edu.br>
2026-09-03 21:26:12 -0300

    variaveis

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,12 @@
 document.getElementById("demo").innerHTML = "God"
+let x, u, z;
+
+x = 5;
+y = 2;
+z = x * y;
+document.getElementById("resultado").innerHTML = z;
+
+let a, b;
+a = 3;
+b = (100+50)*a;
+document.getElementById("resultado2").innerHTML = b;
