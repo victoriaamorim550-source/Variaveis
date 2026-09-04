66188ff4e331b84f1a694e09ad42e0b48c8a573f
VICTORIA AMORIM DOS SANTOS <victoria.asantos13@senacsp.edu.br>
2026-09-03 21:57:33 -0300

    variaveis

--- javascript/script.js
+++ javascript/script.js
@@ -10,3 +10,12 @@ let a, b;
 a = 3;
 b = (100+50)*a;
 document.getElementById("resultado2").innerHTML = b;
+
+function pegarValor(){
+    //Pegando o elemento input pelo id e armazenando em uma variável//
+    let sabrina = document.getElementById("meuInput")
+    //Extrai o valor digitado usando o .value
+    let ValorDigitado = sabrina.value;
+    //Exibir os dados//
+document.getElementById("ola").innerHTML = ValorDigitado;
+}
